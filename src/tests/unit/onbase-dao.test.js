8c32f61d936c351deba297599cf8a5a23051f43a
Alex Ruef <alex.ruef@oregonstate.edu>
2019-10-08 11:03:25 -0700

    fixed lint issues with test structure

--- src/tests/unit/onbase-dao.test.js
+++ src/tests/unit/onbase-dao.test.js
@@ -5,7 +5,7 @@ const chaiAsPromised = require('chai-as-promised');
 const _ = require('lodash');
 const sinon = require('sinon');
 
-const onbaseDao = require('../../api/v1/db/oracledb/onbase-dao')
+const onbaseDao = require('../../api/v1/db/oracledb/onbase-dao');
 const testData = require('./test-data');
 
 chai.should();
@@ -13,15 +13,15 @@ chai.use(chaiAsPromised);
 
 describe('Test onbase-dao', () => {
   beforeEach(() => {
-    const daoStub = sinon.stub(onbaseDao, 'getOnBase')
+    const daoStub = sinon.stub(onbaseDao, 'getOnBase');
     daoStub.returnsArg(0);
   });
   afterEach(() => sinon.restore());
 
   describe('test', () => {
-    it(`dun dun dun`, () => {
+    it('dun dun dun', () => {
       console.log(onbaseDao.getOnBase(0));
       // return onbaseDao.getOnBase(0).should.eventually.be.fulfilled.and.deep.equals(0);
     });
-  })
+  });
 });
