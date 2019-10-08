d13a5ca36211d41d4df414c40515fed16af06443
Alex Ruef <alex.ruef@oregonstate.edu>
2019-10-08 08:58:10 -0700

    added onbase-dao.test.js

---
+++ src/tests/unit/onbase-dao.test.js
@@ -0,0 +1,7 @@
+/* eslint no-unused-vars: 0 */
+// const appRoot = require('app-root-path');
+const chai = require('chai');
+const _ = require('lodash');
+const sinon = require('sinon');
+
+const testData = require('./test-data');
